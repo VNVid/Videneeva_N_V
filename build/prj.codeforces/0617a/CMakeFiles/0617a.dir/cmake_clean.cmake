974d248003b09ee7eb13f1a05fdf40aae83826d0
Nadezhda <vidnav@mail.ru>
2022-02-06 22:28:40 +0300

    easy task

---
+++ build/prj.codeforces/0617a/CMakeFiles/0617a.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "0617a"
+  "0617a.pdb"
+  "CMakeFiles/0617a.dir/0617a.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/0617a.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
